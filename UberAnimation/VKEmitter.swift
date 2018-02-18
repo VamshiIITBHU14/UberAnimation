15b3e708c83dc7832fbcb29620f3ecb552f22ab3
Vamshi Krishna <guduru.vkrishna.civ10@itbhu.ac.in>
2018-02-18 17:43:23 +0530

    Added Core Code for emissions

--- UberAnimation/VKEmitter.swift
+++ UberAnimation/VKEmitter.swift
@@ -7,3 +7,58 @@
 //
 
 import Foundation
+import UIKit
+
+class VKEmitter:NSObject{
+    
+    var emitter = CAEmitterLayer()
+    func emitParticles(superView:UIView, imageNamesArray:[String], stopAfterSeconds:Double, type:Int, anchorView:UIView){
+        var typeOfAnimation = ""
+        switch type {
+        case 1:
+            typeOfAnimation = kCAEmitterLayerPoints
+        case 2:
+            typeOfAnimation = kCAEmitterLayerLine
+        case 3:
+            typeOfAnimation = kCAEmitterLayerRectangle
+        default:
+            typeOfAnimation = kCAEmitterLayerPoints
+        }
+        
+        let rect = CGRect(x: 0, y: anchorView.frame.maxY-anchorView.frame.size.height/2, width: UIScreen.main.bounds.width, height: 0)
+        emitter = CAEmitterLayer()
+        emitter.frame = rect
+        superView.layer.addSublayer(emitter)
+        emitter.emitterShape = typeOfAnimation
+        emitter.emitterSize = rect.size
+        emitter.emitterPosition = CGPoint(x: rect.width/2, y: rect.size.height/2)
+        
+        Timer.scheduledTimer(timeInterval: stopAfterSeconds, target: self, selector: #selector(stopEmitting), userInfo: nil, repeats: false)
+        emitter.emitterCells = (0..<imageNamesArray.count).map({ i in
+            let emitterCell = CAEmitterCell()
+            emitterCell.contents = UIImage(named: imageNamesArray[i])?.cgImage
+            emitterCell.birthRate = 100
+            emitterCell.lifetime = 1.5
+            emitterCell.lifetimeRange = 1
+            emitterCell.yAcceleration = 60.0
+            emitterCell.xAcceleration = -10
+            emitterCell.velocity = 150
+            emitterCell.velocityRange = 200
+            emitterCell.emissionLongitude = -.pi * 0.5
+            emitterCell.emissionRange = .pi * 1
+            emitterCell.redRange = 0.3
+            emitterCell.greenRange = 0.3
+            emitterCell.blueRange = 0.3
+            emitterCell.scale = 0.8
+            emitterCell.scaleRange = 0.8
+            emitterCell.scaleSpeed = -0.15
+            emitterCell.alphaRange = 0.7
+            emitterCell.alphaSpeed = -0.15
+            return emitterCell
+        })
+    }
+    
+    @objc func stopEmitting(){
+        emitter.removeFromSuperlayer()
+    }
+}
